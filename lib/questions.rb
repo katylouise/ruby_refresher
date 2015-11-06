e4f24396d3c5e4fb399aeb5c0202b31e4d3c3596
Rebecca Appleyard <rklappleyard@gmail.com>
2015-11-06 17:09:38 +0000

    Solution to tenth problem

--- lib/questions.rb
+++ lib/questions.rb
@@ -46,12 +46,15 @@ end
 # sort an array of words by their last letter, e.g.
 # ['sky', 'puma', 'maker'] becomes ['puma', 'maker', 'sky']
 def array_sort_by_last_letter_of_word(array)
+  array.sort{ |a, b| a[-1] <=> b[-1] }
 end
 
 # cut strings in half, and return the first half, e.g.
 # 'banana' becomes 'ban'. If the string is an odd number of letters
 # round up - so 'apple' becomes 'app'
 def get_first_half_of_string(string)
+  n = (string.length + 1) / 2
+  string[0, n]
 end
 
 # turn a positive integer into a negative integer. A negative integer
