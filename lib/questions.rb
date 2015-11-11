00088c12102cb3e3d44406c958e99b20f88eb4b5
Rebecca Appleyard <rklappleyard@gmail.com>
2015-11-11 09:34:54 +0000

    Solution to square root of a number

--- lib/questions.rb
+++ lib/questions.rb
@@ -212,10 +212,12 @@ end
 
 # get the square root of a number
 def square_root_of(number)
+  Math.sqrt(number)
 end
 
 # count the number of words in a file
 def word_count_a_file(file_path)
+
 end
 
 # --- tougher ones ---
